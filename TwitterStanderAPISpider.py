6e64f2d0a6fa295aebc68f5bde06f5fc62db42ee
vscodefrommac <1294851493@qq.com>
2025-03-03 00:49:42 +0800

    Merge remote main branch with unrelated histories

--- TwitterStanderAPISpider.py
+++ TwitterStanderAPISpider.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time  # 导入 time 模块
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
