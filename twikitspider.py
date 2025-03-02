133d12438b5affb030a8ec83fe2ad1cbb2c8de9b
vscodefrommac <1294851493@qq.com>
2025-03-03 01:20:33 +0800

    third comiit

--- twikitspider.py
+++ twikitspider.py
@@ -1,5 +1,6 @@
 import asyncio
 from twikit import Client
+import httpx
 
 USERNAME = 'sunny_jamerr'
 EMAIL = '1765423653@qq.com'
@@ -14,5 +15,9 @@ async def main():
         auth_info_2=EMAIL,
         password=PASSWORD
     )
+    tweets = await client.get_user_tweets('elonmusk', 'Tweet')
+    for tweet in tweets:
+        print(tweet.text)
 
 asyncio.run(main())
+
